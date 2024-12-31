4db7d149d70b769dad45210fe66a797a879004c2
Eyal Avisar <eyalavi903@gmail.com>
2024-12-31 15:24:11 +0200

    full review code for dictionaries_HW

--- dictionaries_HW.py
+++ dictionaries_HW.py
@@ -1,4 +1,13 @@
 def sorting_task(dic):
+    """
+    Sorts a dictionary by its values in ascending order.
+
+    Args:
+        dic (dict): Dictionary with keys as tasks and values as deadlines.
+
+    Returns:
+        dict: Dictionary sorted by deadlines.
+    """
     sorted_by_values = dict(sorted(dic.items(), key=lambda item:item[1]))
     return sorted_by_values
 
@@ -6,11 +15,13 @@ def sorting_task(dic):
 task = {"Buy groceries": "2024-12-20", "Pay bills": "2024-12-22","Go to the market": "2024-12-28","Go to sky": "2024-12-19"}
 sorted_task = sorting_task(task)
 
-for i, (key, value) in enumerate(sorted_task.items()):
-    if i < len(sorted_task) - 1:
-        print(f"{key}: {value},")
-    else:
-        print(f"{key}: {value}")
+print(",\n".join([f"{key}: {value}" for key, value in sorted_task.items()]))    ## code review fix  print key value (shortly code)
+
+# for i, (key, value) in enumerate(sorted_task.items()):
+#     if i < len(sorted_task) - 1:
+#         print(f"{key}: {value},")
+#     else:
+#         print(f"{key}: {value}")
         
 
     
