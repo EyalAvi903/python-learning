7dec385d8625d460e9b424bce4ccf53f530538c6
Eyal Avisar <eyalavi903@gmail.com>
2024-12-31 15:19:03 +0200

    soted --> sorted

--- dictionaries_HW.py
+++ dictionaries_HW.py
@@ -1,13 +1,13 @@
 def sorting_task(dic):
-    soted_by_values = dict(sorted(dic.items(), key=lambda item:item[1]))
-    return soted_by_values
+    sorted_by_values = dict(sorted(dic.items(), key=lambda item:item[1]))
+    return sorted_by_values
 
 
 task = {"Buy groceries": "2024-12-20", "Pay bills": "2024-12-22","Go to the market": "2024-12-28","Go to sky": "2024-12-19"}
-sored_task = sorting_task(task)
+sorted_task = sorting_task(task)
 
-for i, (key, value) in enumerate(sored_task.items()):
-    if i < len(sored_task) - 1:
+for i, (key, value) in enumerate(sorted_task.items()):
+    if i < len(sorted_task) - 1:
         print(f"{key}: {value},")
     else:
         print(f"{key}: {value}")
